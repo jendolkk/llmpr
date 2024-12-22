7944e48cc58eb4067aa840a70a97a9d2760722a0
jendolkk <2792789414@qq.com>
2024-12-22 12:10:27 +0800

    model

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+from nltk.metrics.aline import similarity_matrix
 from torch import nn
 from torch.nn import functional as F
 from torch.cuda import temperature
@@ -15,45 +16,49 @@ class AModel(nn.Module):
     self.temperature = nn.Parameter(torch.tensor(temperature))
     self.s_hats = nn.Parameter(torch.zeros(3))
 
-  def forward(self, images, texts_embeddings):
+  def forward(self, images, texts_embeddings, cls, cat):
     visual_embeddings = self.visual_encoder(images)
     # 计算损失函数
     clip_loss = self.calculate_clip_loss(visual_embeddings, texts_embeddings)
-    cls_loss, cls_labels = self.calculate_cls_loss(visual_embeddings, texts_embeddings)
-    cat_loss, cat_labels = self.calculate_cat_loss(visual_embeddings, texts_embeddings)
-
+    cls_loss, cls_labels = self.calculate_loss(visual_embeddings, texts_embeddings, cls)
+    cat_loss, cat_labels = self.calculate_loss(visual_embeddings, texts_embeddings, cat)
     loss3 = torch.tensor([-clip_loss, cls_loss, cat_loss])
     total_loss = (loss3 / self.s_hats.exp()).sum() + self.s_hats.sum()
     return total_loss
 
+  def cross_entropy(self, preds, targets, reduction='none'):
+    log_softmax = nn.LogSoftmax(dim=-1)
+    loss = (-targets * log_softmax(preds)).sum(1)
+    if reduction == "none":
+      return loss
+    elif reduction == "mean":
+      return loss.mean()
+
   def calculate_clip_loss(self, visual_embeddings, text_embeddings):
-    logits = (text_embeddings @ image_embeddings.T) / self.temperature
-    images_similarity = image_embeddings @ image_embeddings.T
+    logits = (text_embeddings @ visual_embeddings.T) / self.temperature
+    images_similarity = visual_embeddings @ visual_embeddings.T
     texts_similarity = text_embeddings @ text_embeddings.T
     targets = F.softmax(
       (images_similarity + texts_similarity) / 2 * self.temperature, dim=-1
     )
-    texts_loss = cross_entropy(logits, targets, reduction='none')
-    images_loss = cross_entropy(logits.T, targets.T, reduction='none')
+    texts_loss = self.cross_entropy(logits, targets, reduction='none')
+    images_loss = self.cross_entropy(logits.T, targets.T, reduction='none')
     loss = (images_loss + texts_loss) / 2.0  # shape: (batch_size)
     return loss.mean()
 
-    # similarities = F.cosine_similarity(visual_embeddings, text_embeddings, dim=1)
-    # logits_per_image = torch.exp(similarities) / torch.sum(torch.exp(similarities), dim=1, keepdim=True)
-    # loss = -torch.log(logits_per_image.gather(1, text_embeddings.argmax(1).unsqueeze(1)).squeeze())
-    # return loss.mean()
-
-  def calculate_cls_loss(self, visual_features, text_features, cls_labels):
+  def calculate_loss(self, visual_embeddings, text_embeddings, labels):
     # 假设cls_labels是类别标签，用于计算基于类别的损失
-    class_logits = torch.matmul(visual_features, text_features.T) / self.temperature
-    class_loss = F.cross_entropy(class_logits, cls_labels)
-    return class_loss, cls_labels
+    logits_per_text = (text_embeddings @ visual_embeddings.T) / self.temperature
+    logits_per_text = logits_per_text.exp()
+    sim_mat = (labels == labels.T).int()
+    # logits_per_img = logits_per_text.T
+    logits2 = logits_per_text * sim_mat
+    # logits2_T = logits2.T
 
-  def calculate_cat_loss(self, visual_features, text_features, cat_labels):
-    # 假设cat_labels是类别标签，用于计算基于头部/尾部类别的损失
-    category_logits = torch.matmul(visual_features, text_features.T) / self.temperature
-    category_loss = F.cross_entropy(category_logits, cat_labels)
-    return category_loss, cat_labels
+    loss_per_text = logits2.sum(1) / logits_per_text.sum(1)
+    loss_per_img = logits2.sum(0) / logits_per_text.sum(0)
+    denominator = sim_mat.sum(0)
+    return -(loss_per_text / denominator).log() + -(loss_per_img / denominator).log()
 
 class VisualBackbone(nn.Module):
   def __init__(self):
