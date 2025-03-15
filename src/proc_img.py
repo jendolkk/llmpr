e2d95611b242f78f979827de6c90e3ea2f260217
jendolkk <2792789414@qq.com>
2025-03-15 20:17:53 +0800

    3152

--- src/proc_img.py
+++ src/proc_img.py
@@ -3,31 +3,38 @@ import os
 import torch
 from PIL import Image
 import numpy as np
+from torchvision.transforms import v2
 from modelb import AModel, VisualBackbone
 import faiss
 import constant
 
 d = constant.embedding_dim
 index = faiss.IndexFlatL2(d)
-id2filename = []
-img_names = []
-imgf_np = []
+id2filename = {}
+filename2id = {}
+# imgf_np = []
 
-model = AModel(VisualBackbone())
-model.load_state_dict(torch.load('./model.pth'))
+model = AModel(VisualBackbone('vit'))
+model.load_state_dict(torch.load('../models/model19.pth'))
 model.eval()
 
-transf = torch.transform.v2.ToTensor()
-with torch.no_grad():
-  with open(constant.json_file, 'r') as f:
-    dicts = json.load(f)
-    for mp in dicts:
-      name = mp['subfigure_file']
-      image = Image.open(os.path.join(constant.img_dir, name))
-      img_feature = model.visual_encoder(transf(image).unsqueeze(0)).squeeze(0).numpy()
-      index.add(img_feature)
-      imgf_np.append(img_feature)
-      img_names.append(name)
+trans = v2.Compose([
+  v2.Resize((224, 224)),
+  v2.ToTensor(),
+])
 
-np.save(constant.imgf_np, imgf_np)
+with torch.no_grad():
+  with open(constant.test_json, 'r') as f:
+    dicts = json.load(f)
+    for i, mp in enumerate(dicts):
+      name = mp['subfigure_file']
+      id2filename[i] = name
+      filename2id[name] = i
+
+      image = Image.open(os.path.join(constant.img_dir, name))
+      img_feature = model.visual_encoder(trans(image).unsqueeze(0)).squeeze(0).numpy()
+      index.add(img_feature)
+      # imgf_np.append(img_feature)
+
+# np.save(constant.imgf_np, imgf_np)
 faiss.write_index(index, constant.faiss_file)
